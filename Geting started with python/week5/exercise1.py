058413149017861a132a0fd5f4b1456eabfd2686
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-23 09:00:14 -0500

    fixed

--- Geting started with python/week5/exercise1.py
+++ Geting started with python/week5/exercise1.py
@@ -1,19 +1,21 @@
 hrs = input("Enter Hours:")
 pph = input("Enter rate per hour:")
 h = float(hrs)
-pr = float(10.50)
+pr = float(pph)
 
 over_hp = 1.5
 
-if h <= 40:
+if (h <= 40):
     payment = h * pr
     print(payment)
-elif h > 40:
-    regular_hours = 40
-    overtime_hours = h - regular_hours
-    regular_payment = regular_hours * pr
-    overtime_payment = overtime_hours * pr * over_hp
-    total_payment = regular_payment + overtime_payment
-    print(total_payment)
+
+elif (h > 40):
+    regular_hr = 40;
+    regular_payment = 40 * pr
+    over_hour = h - regular_hr
+    over_hour_payment = over_hour * pr * over_hp
+    total = regular_payment + over_hour_payment
+    print(total)
+
 else:
     print("Invalid entry")
