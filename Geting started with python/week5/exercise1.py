35588eb74db80a3d537d8de5626a31adce3664bf
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-23 08:49:52 -0500

    fixed

--- Geting started with python/week5/exercise1.py
+++ Geting started with python/week5/exercise1.py
@@ -5,16 +5,15 @@ pr = float(10.50)
 
 over_hp = 1.5
 
-if (h <= 40):
+if h <= 40:
     payment = h * pr
     print(payment)
-
-elif (h > 40):
-    payment = 40.0 * pr
-    opayment = (40 - h) * over_hp
-    print(opayment)
-    t_Payment = payment + opayment
-    print(t_Payment)
-
+elif h > 40:
+    regular_hours = 40
+    overtime_hours = h - regular_hours
+    regular_payment = regular_hours * pr
+    overtime_payment = overtime_hours * pr * over_hp
+    total_payment = regular_payment + overtime_payment
+    print(total_payment)
 else:
     print("Invalid entry")
