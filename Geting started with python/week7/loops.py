60cd7aa06fa17dde21c5cb55bf490dfd84e79e62
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-25 03:27:35 -0500

    done

--- Geting started with python/week7/loops.py
+++ Geting started with python/week7/loops.py
@@ -3,4 +3,8 @@ largest_so_far = -1
 print("Before", largest_so_far)
 
 for the_num in [9, 42, 23, 455, 64, 35644, 33, 31, 3, 43, 89, 79,] :
-    if (largest_so_far < )
+    if (largest_so_far < the_num):
+        largest_so_far = the_num
+    print(largest_so_far, the_num)
+
+    print("After", largest_so_far)
