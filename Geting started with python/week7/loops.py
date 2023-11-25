866f2b9c106d4b83672931e17d07eb8d6752c9e1
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-25 03:25:58 -0500

    first commit

--- Geting started with python/week7/loops.py
+++ Geting started with python/week7/loops.py
@@ -1,3 +1,6 @@
-largest_so_fat = -1
+largest_so_far = -1
 
-print("Before", largest_so_fat);
+print("Before", largest_so_far)
+
+for the_num in [9, 42, 23, 455, 64, 35644, 33, 31, 3, 43, 89, 79,] :
+    if (largest_so_far < )
