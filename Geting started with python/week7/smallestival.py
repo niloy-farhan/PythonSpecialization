187973fbedbb25017d950924d70c420165f79f15
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-28 05:03:18 -0500

    update

--- Geting started with python/week7/smallestival.py
+++ Geting started with python/week7/smallestival.py
@@ -4,7 +4,7 @@ print("Before")
 for value in [9, 42, 23, 455, 64, 35644, 33, 31, 3, 43, 89, 79,] :
     if smallest is None :
         smallest = value
-        print("smallest")
+        print("smallest", smallest)
     elif value < smallest :
         smallest = value
     print(smallest, value)
