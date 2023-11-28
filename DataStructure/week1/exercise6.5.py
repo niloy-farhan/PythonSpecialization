8a216d5a28535a2150fc5097512b3a67d8b60ce9
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-11-28 03:12:59 -0500

    new

--- DataStructure/week1/exercise6.5.py
+++ DataStructure/week1/exercise6.5.py
@@ -1,9 +1,8 @@
-text = "X-DSPM-Confidence:      0.8475"
+text = "X-DSPM-Confidence: 0.8475"
 
 colon_position = text.find(":")
-
-numeric_part = text[colon_position + 1:].strip()
-
-confidence_value = float(numeric_part)
-
-print(confidence_value)
+print(colon_position)
+piece = text[colon_position+2:]
+print(piece)
+value = float(piece)
+print(value)
