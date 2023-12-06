94a55b9a212be8bf0179affcee9e1f20d9423faf
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-06 08:57:51 -0500

    update

--- DataStructure/week5/exercise9.4.py
+++ DataStructure/week5/exercise9.4.py
@@ -17,8 +17,10 @@ for line in handle:
     for w in words:
         di[w] = di.get(w, 0) + 1
 largest = -1
+theword = None
 for k, v in di.items():
     print(k, v)
     if v > largest:
         largest = v
-print('Done', largest)
+        theword = k
+print('Done', theword, largest)
