1aabda3b959683db4a030a00ca55ba74b45d5248
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-11 04:22:18 -0500

    update

--- DataStructure/week5/compoundDataStructure.py
+++ DataStructure/week5/compoundDataStructure.py
@@ -1,9 +1,12 @@
-elements = {'hydrogen': {'number': 1,
-                         'weight': 1.00794,
-                         'symbol': 'H'},
-            'helium': {'number': 2,
-                       'weight': 4.002602,
-                       'symbol': 'He'}
+elements = {"hydrogen": {"number": 1,
+                         "weight": 1.00794,
+                         "symbol": 'H'},
+            "helium": {"number": 2,
+                       "weight": 4.002602,
+                       "symbol": "He"},
+            "oxygen": {"number": 8,
+                       "weight": 15.999,
+                       "symbol": "O"}
 }
 
 print(elements['helium']['weight'])
