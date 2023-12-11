569927b9954bfedcc8a23e6a86fe84e75a4959f2
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-11 04:36:57 -0500

    update

--- DataStructure/week5/practice.py
+++ DataStructure/week5/practice.py
@@ -1,7 +1,24 @@
-card_deck = {24, 9, 8, 4, 6, 3, 44, 3}
-hand = []
+# card_deck = {24, 9, 8, 4, 6, 3, 44, 3}
+# hand = []
+#
+# while sum(hand) <= 34:
+#     hand.append(card_deck.pop())
+# hello = sum(hand)
+# print(hand, hello)
 
-while sum(hand) <= 34:
-    hand.append(card_deck.pop())
-hello = sum(hand)
-print(hand, hello)
+headlines = ["Local Bear Eaten by Man",
+             "Legislature Announces New Laws",
+             "Peasant Discovers Violence Inherent in System",
+             "Cat Rescues Fireman Stuck in Tree",
+             "Brave Knight Runs Away",
+             "Papperbok Review: Totally Triffic"]
+
+news_ticker = ""
+for headline in headlines:
+    news_ticker += headline + " "
+    if len(news_ticker) >= 140:
+        news_ticker = news_ticker[:150] + "..."
+        break
+
+
+print(news_ticker)
