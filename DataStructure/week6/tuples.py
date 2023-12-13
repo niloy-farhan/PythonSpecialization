c3d15879178465dd89a2cbb4e78a3a47f7f5ccd5
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-13 09:25:52 -0500

    update

--- DataStructure/week6/tuples.py
+++ DataStructure/week6/tuples.py
@@ -6,10 +6,12 @@ for line in fhand:
     for word in words:
         counts[word] = counts.get(word, 0) + 1
     lst = list()
-    for key, val in counts.items():
-        newTup = (val, key)
-        lst.append(newTup)
-    lst = sorted(lst, reverse=True)
+    # for key, val in counts.items():
+    #     newTup = (val, key)
+    #     lst.append(newTup)
+    # lst = sorted(lst, reverse=True)
+    #
+    # for val, key in lst[:10]:
+    #     print(key, val)
 
-    for val, key in lst[:10]:
-        print(key, val)
+    print(sorted([(v, k) for k, v in counts.items()]))
