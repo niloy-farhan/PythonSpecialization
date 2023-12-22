4f43b027275846757a33238411207160872be7ee
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-22 05:18:22 -0500

    update

---
+++ DatabaseWithPython/class.py
@@ -0,0 +1,19 @@
+class party_animal:
+    x = 0
+
+    def __init__(self):
+        print("I am constructed")
+
+    def party(self):
+        self.x = self.x + 1
+        print("So far", self.x)
+
+    def __del__(self):
+        print("I am destructed", self.x)
+
+
+an = party_animal()
+an.party()
+an.party()
+an = 42
+print('an contains', an)
